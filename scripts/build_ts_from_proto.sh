425a62cc48890180b89bd0731c8e07de486c0aa4
Karolis Gudiškis <karolisgud@gmail.com>
2023-03-09 11:27:24 +0200

    Use text instead of binary

--- scripts/build_ts_from_proto.sh
+++ scripts/build_ts_from_proto.sh
@@ -4,5 +4,5 @@ protoc --plugin=./node_modules/ts-proto/protoc-gen-ts_proto \
 --ts_proto_out="./src/generated" \
 --ts_proto_opt=esModuleInterop=true \
 --js_out=import_style=commonjs,binary:"./src/generated" \
---grpc-web_out=import_style=typescript,mode=grpcweb:"./src/generated" \
+--grpc-web_out=import_style=typescript,mode=grpcwebtext:"./src/generated" \
 protos/*.proto
