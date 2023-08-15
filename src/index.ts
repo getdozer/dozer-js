c6b2ec0dff0ff3bedbe059114f8feca9028428ab
jianxiangxun <xunjianxiang@aliyun.com>
2023-08-15 23:20:39 +0800

    fix: remove export types_pb from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 export * from './client';
+export * from './generated/protos/common';
 export * from './generated/protos/CommonServiceClientPb';
 export * from './generated/protos/HealthServiceClientPb';
 export * from './generated/protos/IngestServiceClientPb';
-export * from './generated/protos/common';
-export * from './generated/protos/types_pb';
 export * from './query_helper';
 
