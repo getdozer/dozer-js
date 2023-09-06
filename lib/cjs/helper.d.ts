b967dce3f24031d6df95c29ec47c40eefd376a5e
VG <vivekg342@gmail.com>
2023-09-07 02:24:17 +0800

    chore: reorganize directory

--- lib/cjs/helper.d.ts
+++
@@ -1,6 +0,0 @@
-import { FieldDefinition, Value } from "./generated/protos/types_pb.js";
-export declare class RecordMapper {
-    private fields;
-    constructor(fields: FieldDefinition[]);
-    mapRecord(values: Value[]): Object;
-}
--- lib/cjs/ingest_client.d.ts
+++
@@ -1,8 +0,0 @@
-import { IngestArrowRequest, IngestRequest } from "./generated/protos/ingest_pb";
-export declare class DozerIngestClient {
-    private readonly endpoint;
-    private service;
-    constructor(endpoint: string, server?: string);
-    ingest_raw(request: IngestRequest): Promise<import("./generated/protos/ingest_pb").IngestResponse>;
-    ingest_arrow(request: IngestArrowRequest): Promise<import("./generated/protos/ingest_pb").IngestResponse>;
-}
