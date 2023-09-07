82ebcf2a6f52d5de17b4032147af7a31026aa34e
VG <vivekg342@gmail.com>
2023-09-07 19:51:47 +0800

    chore: fix exports

--- packages/js-client/src/index.ts
+++ packages/js-client/src/index.ts
@@ -1,7 +1,19 @@
 export * from './client';
-export * from './generated/protos/common';
-export * from './generated/protos/CommonServiceClientPb';
-export * from './generated/protos/HealthServiceClientPb';
-export * from './generated/protos/IngestServiceClientPb';
+export * as common from './generated/protos/common';
+export * as common_pb from './generated/protos/common_pb';
+
+export * as types from './generated/protos/types';
+export * as types_pb from './generated/protos/types_pb';
+
+export * as ingest from './generated/protos/ingest';
+export * as ingest_pb from './generated/protos/ingest_pb';
+
+
+export * as health from './generated/protos/health';
+export * as health_pb from './generated/protos/health_pb';
+
+export * as auth from './generated/protos/auth';
+export * as auth_pb from './generated/protos/auth_pb';
+
 export * from './query_helper';
 
