6c6caa4fcc212b7ca1de8ab6b0a772b5145f2976
jianxiangxun <xunjianxiang@getdozer.io>
2023-10-17 02:47:44 +0800

    feat: refactor react datatable with react-virtuoso

--- packages/react-example/src/App.tsx
+++ packages/react-example/src/App.tsx
@@ -1,63 +1,38 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Alert, Box } from '@mui/material';
-import { DozerProvider, useDozerEndpoint } from "@dozerjs/dozer-react"
 import { types_pb } from '@dozerjs/dozer';
+import { DozerProvider, useDozerWatch } from '@dozerjs/dozer-react';
+import { DozerTable } from './DataTable';
+import { Box } from '@mui/material';
 
 
-function DozerTable () {
-  const { fields, records, error } = useDozerEndpoint('trips_cache', { watch: 0 })
+function Endpoints () {
+  const options = [
+    {
+      endpoint: 'trips_cache',
+      eventType: types_pb.EventType.ALL,
+    },
+  ];
 
-  if (error) {
-    return (
-      <Box sx={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100%',
-      }}>
-        <Alert severity="error">{error.message}</Alert>
-      </Box>
-    )
-  }
-  
+  const { stream } = useDozerWatch(options);
+
   return (
-    <TableContainer component={Paper} sx={{ height: '100%' }}>
-      <Table aria-label="dozer table">
-        <TableHead>
-          <TableRow>
-            {fields?.map((field, idx) => (
-              <TableCell key={idx}>{field.getName()}</TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {records.map((row: any, idx) => {
-            return (
-              <TableRow key={idx}>
-                {fields?.map(field => {
-                  let val = row[field.getName()]
-                  if (field.getTyp() === types_pb.Type.JSON) {
-                    val = JSON.stringify(val);
-                  } else {
-                    val = val?.toString()
-                  }
-                  return (<TableCell component="th" scope="row" key={field.getName()}>
-                    {val}
-                  </TableCell>)
-                })}
-              </TableRow>
-            )
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Box sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '1rem',
+    }}>
+      {
+        options.map(option => <DozerTable key={option.endpoint} stream={stream} endpoint={option.endpoint}/>)
+      }
+    </Box>
   )
 }
 
-
 function App() {
   return (
-    <DozerProvider>
-      <DozerTable />
+    <DozerProvider value={{
+      serverAddress: 'http://127.0.0.1:62998'
+    }}>
+      <Endpoints />
     </DozerProvider>
   )
 }
---
+++ packages/react-example/src/DataTable.tsx
@@ -0,0 +1,93 @@
+import { types_pb } from "@dozerjs/dozer";
+import { useDozerQuery } from "@dozerjs/dozer-react";
+import { Alert, Box, Paper, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import { ClientReadableStream } from "grpc-web";
+import { forwardRef } from "react";
+import { TableComponents, TableVirtuoso } from "react-virtuoso";
+
+export function DozerTable(props: {
+  stream: ClientReadableStream<types_pb.Operation>;
+  endpoint: string;
+}) {
+  const { fields, records, error, connect } = useDozerQuery(props.endpoint, {});
+  connect(props.stream);
+
+  if (error) {
+    return (
+      <Box sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '500px',
+      }}>
+        <Alert severity="error">{error.message}</Alert>
+      </Box>
+    )
+  }
+
+  const VirtuosoTableComponents: TableComponents<object> = {
+    Table: (props) => (
+      <Table {...props} sx={{ borderCollapse: 'separate', tableLayout: 'fixed' }} />
+    ),
+    TableHead,
+    TableBody: forwardRef<HTMLTableSectionElement>((props, ref) => (
+      <TableBody {...props} ref={ref} />
+    )),
+    TableRow: ({ item: _item, ...props }) => <TableRow {...props}>
+      {fields?.map((field: types_pb.FieldDefinition) => {
+        let val = (_item as any)[field.getName()]
+        if (field.getTyp() === types_pb.Type.JSON) {
+          val = JSON.stringify(val);
+        } else {
+          val = val?.toString()
+        }
+        return (<TableCell component="th" scope="row" key={field.getName()}>
+          {val}
+        </TableCell>)
+      })}
+    </TableRow>,
+
+  };
+
+  function FixedHeaderContent() {
+    return (
+      <TableRow>
+        {fields.map((column: types_pb.FieldDefinition) => (
+          <TableCell
+            key={column.getName()}
+            variant="head"
+            sx={{
+              backgroundColor: 'background.paper',
+            }}
+          >
+            {column.getName()}
+          </TableCell>
+        ))}
+      </TableRow>
+    );
+  }
+
+  function RowContent(_index: number, row: any) {
+    return (
+      <>
+        {fields.map((column: types_pb.FieldDefinition) => (
+          <TableCell key={column.getName()}
+          >
+            {row[column.getName()]}
+          </TableCell>
+        ))}
+      </>
+    );
+  }
+
+  return (
+    <Paper sx={{ height: '500px' }}>
+      <TableVirtuoso
+        data={records}
+        components={VirtuosoTableComponents}
+        fixedHeaderContent={FixedHeaderContent}
+        itemContent={RowContent}
+      />
+    </Paper>
+  )
+}
